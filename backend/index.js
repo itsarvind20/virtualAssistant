ca0e7df3bde0e4984b30e81aaebcbccb186dbc50
itsarvind20 <arvirawat454@gmail.com>
2025-10-06 18:50:48 +0530

    sign in and login backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDb from "./config/db.js"
 dotenv.config()
+const app = express()
 
-const app=express()
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
     res.send("hii")
 })
 app.listen(port,()=>{
+    connectDb()
     console.log("server started")
 })
