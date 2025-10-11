e5762c83de18460d17ba326dfc6f11f5ea90482e
itsarvind20 <arvirawat454@gmail.com>
2025-10-11 15:02:09 +0530

    backend 1st phase done

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
 import express from "express"
 import dotenv from "dotenv"
-import connectDb from "./config/db.js"
 dotenv.config()
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import userRouter from "./routes/user.routes.js"
+import geminiResponse from "./gemini.js"
+
+
 const app = express()
-
 const port = process.env.PORT || 3000
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 
-app.get("/",(req,res)=>{
-    res.send("hii")
-})
 app.listen(port,()=>{
     connectDb()
     console.log("server started")
